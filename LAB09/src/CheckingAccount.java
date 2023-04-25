499c2c7b67a9cffbb973500a2e4589feefdaedaf
Pakin Junjumlong <124805125+EIKZAZ@users.noreply.github.com>
2023-04-25 23:57:00 +0700

    Add files via upload

---
+++ LAB09/src/CheckingAccount.java
@@ -0,0 +1,43 @@
+
+public class CheckingAccount extends Account {
+    private double credit;
+    public CheckingAccount() {
+        super(0, "");
+    }
+    public CheckingAccount(double balance, String name, double credit) {
+        super(balance, name);
+        this.credit = credit;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        if (credit < 0) {
+            System.out.println("Input number must be a positive integer.");
+        }
+        this.credit = credit;
+    }
+    @Override
+    public void withdraw(double a) throws WithdrawException{
+        if (balance - a  > 0) {
+            balance -= a;
+            System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit + ".");
+        }else if (balance - a < 0 && credit > 0) {
+            System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit + "." );
+            this.balance = 0;
+            this.credit = credit - a;
+        } else {
+            throw new WithdrawException("Account " +name+ " has not enough money.");
+            //System.out.println("Not enough money!");
+        }
+    }
+    public void withdraw(String a) throws WithdrawException{
+        this.withdraw(Double.valueOf(a));
+    }
+    @Override
+    public String toString() {
+        return "The " + name + " account has " + balance + " baht and " + credit + " credits.";
+    }
+}
